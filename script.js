debe38d3a1871236324dbe363bdac5b1720bb0fa
Silvineey <lucreciasilvio@hotmail.com>
2025-10-21 09:59:26 -0300

    mudanças
    mudanças

---
+++ script.js
@@ -0,0 +1,43 @@
+let indice=0
+const saida=document.getElementById("saida")
+const cookie=document.querySelector(".cookie") 
+const whey=document.querySelector(".whey")
+function contador(){
+    indice++
+    saida.innerHTML=indice
+    cookie.animationDelay="2s"
+    if(indice===2){
+    cookie.style.transform="scale(1.1)"
+    saida.style.marginTop="1px"
+    }
+    else if(indice===5){
+    cookie.style.transform="scale(1.2)"
+    saida.style.marginTop="2px"
+    }
+    else if(indice===8){
+        cookie.style.transform="scale(1.3)"
+        saida.style.marginTop="5px"
+    }
+    else if(indice===10){
+        cookie.style.transform="scale(1.5)"
+        saida.style.marginTop="8px"
+        setTimeout(() => {
+            saida.style.color="black"
+            cookie.style.display="none"
+            whey.style.display="block"
+            document.body.style.backgroundColor="#343e72"
+        }, 1000);
+    }
+    else if(indice===20){
+        document.body.style.backgroundColor="#475497ff"
+        setTimeout(() => {
+            whey.style.transform="translatex(-300px)"
+        }, 1000);
+    }
+    else if(indice===30){
+        document.body.style.backgroundColor="#475497ff"
+        setTimeout(() => {
+            whey.style.transform="translatex(300px)"
+        }, 1000);
+    }
+}
